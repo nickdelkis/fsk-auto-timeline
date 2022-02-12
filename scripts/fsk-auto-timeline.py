59a8eca575bb2a704614f455fc23278974ca50e6
Nick Delkis <nickdelkis@gmail.com>
2022-02-12 21:14:13 +0200

    Create data_input function (needs completion)

--- scripts/fsk-auto-timeline.py
+++ scripts/fsk-auto-timeline.py
@@ -4,3 +4,27 @@ number of plates per transformation, starting date, are there available competen
 Output: A protocol with dates, exported in a txt or doc file for further curation or PDF """
 
 #import
+
+def data_input():
+""" Ask for input from the user, to create constructs """
+	# Number of constructs
+	construct_number = int(input(f'Number of constructs for transformation:'))
+	# Number of plates per construct to calculate plates needed for transfers, 
+	plates_per_construct = int(input(f'Number of plates per construct for transformation:'))
+	# Construct Names
+	print(f"Please enter your constructs' names")
+	for n in construct_number:
+		str(input(f'Construct {n}: '))
+	# agrobacterium antibiotic resistance gene
+	agro_antib_name = str(input(f'Antibiotic name used for Agrobacterium:'))
+	# agrobacterium antibiotic resistance concentration ug/mL
+	agro_antib_conc = int(input(f'Antibiotic concentration used for Agrobacterium:'))
+	# Competent cells available? 
+	agro_comp_cells = str(input(f'Are Agrobacterium competent cells available?'))
+	# agrobacterium antibiotic resistance gene
+	fsk_antib_name = str(input(f'Antibiotic name used for FsK selection:'))
+	# agrobacterium antibiotic resistance concentration ug/mL
+	fsk_antib_conc = int(input(f'Antibiotic concentration used for FsK selection (ug/mL):'))
+	# Number of constructs
+	int(input())
+	#TODO: ask for FsK strain, resistance , starting date
