7798a9efab56ee9e279fa8cdbba98cf43b33f86e
Nick Delkis <nickdelkis@gmail.com>
2022-02-12 19:45:53 +0200

    Start working

--- scritps/fsk-auto-timeline.py
+++ scritps/fsk-auto-timeline.py
@@ -1,4 +1,6 @@
 """ Purpose of this script is to automate protocol creation for Agrobacterium mediated transformation of Fusarium solani strain K
 Input: Several parameters that are required to create the protocol, such as number of constructs, antibiotic resistance, 
 number of plates per transformation, starting date, are there available competent cells or not
-Output: A protocol with dates, exported in a txt or doc file for further curation or PDF 
+Output: A protocol with dates, exported in a txt or doc file for further curation or PDF """
+
+#import
