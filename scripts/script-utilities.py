249ab20424160c0de4646766a41feba71925f1cd
Nick Delkis <nickdelkis@gmail.com>
2022-02-15 20:23:04 +0200

    Complete data_input() function, create calculate and create table functions

--- scripts/script-utilities.py
+++ scripts/script-utilities.py
@@ -9,15 +9,17 @@ import sys
 from datetime import datetime, timedelta
 from dateutil import parser
 from tabulate import tabulate
+from math import ceil
+import pandas as pd
 
 
 class Construct:
     def __init__(self):
         self.name = ""
         self.agro_antib_name = ""
-        self.agro_antib_conc = ""
+        self.agro_antib_conc = 0
         self.fsk_antib_name = ""
-        self.fsk_antib_conc = ""
+        self.fsk_antib_conc = 0
         self.fsk_wt_strain = ""
         self.fsk_other_strain = ""
         self.fsk_other_antib_name = ""
@@ -50,16 +52,18 @@ class Construct:
             self.fsk_other_strain = str(input(f'Name for parent FsK strain used: '))
             fsk_other_antib_name = str(input(f'Antibiotic name used for parent FsK strain selection: '))
             if self.fsk_antib_name == fsk_other_antib_name:
-                print(f'Warning!: Parent strain {self.fsk_other_strain} also has resistance to {self.fsk_antib_name}.')
+                print(f'Error: Parent strain {self.fsk_other_strain} also has resistance to {self.fsk_antib_name}.')
                 sys.exit(1)
         else:
             print(f'Please answer yes or no.')
             sys.exit(1)
 
 
+
 def data_input():
     """ Ask for input from the user, to customize protocol """
     # Number of constructs
+    global construct_number
     construct_number = int(input(f'Number of constructs for transformation (e.g. 5): '))
     if construct_number > 0:
         print(f"Please enter your constructs' names")
@@ -67,33 +71,50 @@ def data_input():
         for n in range(construct_number):
             c = f'c{n+1}'
             construct_list.append(c)
-            print(construct_list)
         for c in construct_list:
             c = Construct()
             c.add()
+            # TODO: create a dataframe using pandas with each Construct object in a separate column
     else:
         print(f'Warning! Please choose a valid construct number.')
         sys.exit(1)
 
     # Number of plates per construct to calculate plates needed for transfers and initial transformations
+    global plates_per_construct
     plates_per_construct = int(input(f'Number of plates per construct for transformation (e.g. 5): '))
-    # Calculate plates and membranes needed per transfer/transformation
+
+    # Calculate plates per transfer/transformation
     global plate_batch
     plate_batch = construct_number * plates_per_construct + 10
+    # CN calculate membranes for transformation
     global membranes
     membranes = construct_number * plates_per_construct + 5
+    # calculate IM liquid medium required to go through all necessary steps, plus overage
+    global im
+    im = construct_number*60
 
     # Competent cells available? Will include one week of competent cell preparation if answer is no
+    global agro_comp_cells
     agro_comp_cells = str(input(f'Are Agrobacterium competent cells available? Answer yes or no : '))
-    if agro_comp_cells == 'yes':
-        pass
-    else:
+    if not agro_comp_cells == 'yes' or agro_comp_cells == 'no':
         print(f'Please answer yes or no.')
         sys.exit(1)
 
     # Ask for starting date (uses dateutil parser so strict format not required)
     starting_date = parser.parse(input("Enter experiment's starting date: "))
     print(starting_date)
+
+
+
+def round_up(n, decimals=-2):
+    """Round up numbers for media preparation, when calculating volumes."""
+    multiplier = 10 ** decimals
+    return ceil(n * multiplier) / multiplier
+
+
+def calculate():
+    """Calculate all required components for performing the protocol.
+    This contains plates, membranes, media, media components. Also uses round_up() to make some volumes look better."""
 
 
 def create_table():
@@ -103,3 +124,11 @@ def create_table():
 data_input()
 print(plate_batch)
 print(membranes)
+
+
+def add_week0():
+    # To be used for adding Week 0 (Competent cell preparation) in final protocol
+    if agro_comp_cells == 'yes':
+        pass
+    elif agro_comp_cells == 'no':
+        # TODO: Make protocol create a Week 0 for competent cell prep
