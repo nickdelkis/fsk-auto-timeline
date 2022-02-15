96819907612d34e1c9958ac22e67101ddd9ff545
Nick Delkis <nickdelkis@gmail.com>
2022-02-15 09:28:08 +0200

    Complete data_input() function, create calculate and create table functions

--- scripts/script-utilities.py
+++ scripts/script-utilities.py
@@ -8,92 +8,98 @@ or PDF ready for print. """
 import sys
 from datetime import datetime, timedelta
 from dateutil import parser
+from tabulate import tabulate
 
 
 class Construct:
-	def __init__(self):
-		self.con_name = ""
-		self.agro_antib_name = ""
-		self.agro_antib_conc = ""
-		self.fsk_antib_name = ""
-		self.fsk_antib_conc = ""
+    def __init__(self):
+        self.name = ""
+        self.agro_antib_name = ""
+        self.agro_antib_conc = ""
+        self.fsk_antib_name = ""
+        self.fsk_antib_conc = ""
+        self.fsk_wt_strain = ""
+        self.fsk_other_strain = ""
+        self.fsk_other_antib_name = ""
+
+    def add(self):
+        # Construct x Name
+        self.name = str(input(
+            f'Construct name: '))
+        # agrobacterium antibiotic resistance
+        self.agro_antib_name = str(input(
+            f'Antibiotic used for selection of Agrobacterium strain containing {self.name}: '))
+        # agrobacterium antibiotic resistance concentration ug/mL
+        self.agro_antib_conc = int(input(
+            f'Antibiotic concentration used for selection of Agrobacterium strain containing {self.name} (ug/mL): '))
+        # FsK antibiotic resistance gene for transformation
+        self.fsk_antib_name = str(input(
+            f'Antibiotic name used for FsK selection during transformation: '))
+        # fsk antibiotic resistance concentration ug/mL
+        self.fsk_antib_conc = int(input(
+            f'Antibiotic concentration used for FsK selection during transformation (ug/mL): '))
+        # Ask if fsk strain is WT or not
+        self.fsk_wt_strain = str(input(f'Are you using FsK WT (NEK) as parent strain? Answer yes or no: '))
+        if self.fsk_wt_strain == "yes":
+            # if WT strain, nothing needs to be asked (no extra/competing antibiotic)
+            pass
+        # if uses other fsk strain,
+        elif self.fsk_wt_strain == "no":
+            # if other WT strain, then ask the name (for protocol) and specify antibiotic
+            # to see if they compete with current selection
+            self.fsk_other_strain = str(input(f'Name for parent FsK strain used: '))
+            fsk_other_antib_name = str(input(f'Antibiotic name used for parent FsK strain selection: '))
+            if self.fsk_antib_name == fsk_other_antib_name:
+                print(f'Warning!: Parent strain {self.fsk_other_strain} also has resistance to {self.fsk_antib_name}.')
+                sys.exit(1)
+        else:
+            print(f'Please answer yes or no.')
+            sys.exit(1)
 
 
 def data_input():
-	""" Ask for input from the user, to customize protocol """
-	# Number of constructs
-	construct_number = int(input(f'Number of constructs for transformation (e.g. 5): '))
-	if construct_number > 0:
-		print(f"Please enter your constructs' names")
-		# create empty lists and dicts
-		con_name_lst = []
-		agro_antib_lst = []
-		agro_antib_conc_lst = []
-		agro_antib_name_conc_dict = {}
-		con_agro_antib_dict = {}
-		con_fsk_antib_dict = {}
-		fsk_antib_name_conc_dict = {}
-		# iterate as many times as constructs to ask name, antibiotic and concentration
-		# for selection and add the information to lists and dicts
-		for n in range(construct_number):
-			# Construct x Name
-			name = str(input(f'Construct {n+1} name: '))
-			# append name to list of construct names
-			con_name_lst.append(name)
-			# agrobacterium antibiotic resistance
-			agro_antib_name = str(input(f'Antibiotic used for selection of Agrobacterium strain containing {name}: '))
-			# append antibiotic name to list of antibiotic names
-			agro_antib_lst.append(agro_antib_name)
-			# agrobacterium antibiotic resistance concentration ug/mL
-			agro_antib_conc = int(input(
-				f'Antibiotic concentration used for selection of Agrobacterium strain containing {name} (ug/mL): '))
-			# append antibiotic concentration to list of antibiotic concentrations
-			agro_antib_conc_lst.append(agro_antib_conc)
-			# pair construct name with agro antibiotic and antibiotic with concentration
-			con_agro_antib_dict.update({name: agro_antib_name})
-			agro_antib_name_conc_dict.update({agro_antib_name: agro_antib_conc})
-			# FsK antibiotic resistance gene for transformation
-			fsk_antib_name = str(input(f'Antibiotic name used for FsK selection during transformation: '))
-			# fsk antibiotic resistance concentration ug/mL
-			fsk_antib_conc = int(input(
-				f'Antibiotic concentration used for FsK selection during transformation (ug/mL): '))
-			# pair construct name fsk antibiotic and antibiotic with concentration
-			con_fsk_antib_dict.update({name: fsk_antib_name})
-			fsk_antib_name_conc_dict.update({fsk_antib_name: fsk_antib_conc})
-	else:
-		print(f'Warning! Please choose a valid construct number.')
-		sys.exit(1)
+    """ Ask for input from the user, to customize protocol """
+    # Number of constructs
+    construct_number = int(input(f'Number of constructs for transformation (e.g. 5): '))
+    if construct_number > 0:
+        print(f"Please enter your constructs' names")
+        construct_list = []
+        for n in range(construct_number):
+            c = f'c{n+1}'
+            construct_list.append(c)
+            print(construct_list)
+        for c in construct_list:
+            c = Construct()
+            c.add()
+    else:
+        print(f'Warning! Please choose a valid construct number.')
+        sys.exit(1)
 
-	# Number of plates per construct to calculate plates needed for transfers and initial transformations
-	plates_per_construct = int(input(f'Number of plates per construct for transformation: '))
+    # Number of plates per construct to calculate plates needed for transfers and initial transformations
+    plates_per_construct = int(input(f'Number of plates per construct for transformation (e.g. 5): '))
+    # Calculate plates and membranes needed per transfer/transformation
+    global plate_batch
+    plate_batch = construct_number * plates_per_construct + 10
+    global membranes
+    membranes = construct_number * plates_per_construct + 5
 
-	# Competent cells available? Will include one week of competent cell preparation if answer is no
-	agro_comp_cells = str(input(f'Are Agrobacterium competent cells available? Answer yes or no : '))
+    # Competent cells available? Will include one week of competent cell preparation if answer is no
+    agro_comp_cells = str(input(f'Are Agrobacterium competent cells available? Answer yes or no : '))
+    if agro_comp_cells == 'yes':
+        pass
+    else:
+        print(f'Please answer yes or no.')
+        sys.exit(1)
 
-	# Ask if FsK strain is WT or other
-	fsk_strain_wt = str(input(f'Are you using FsK WT (NEK) as parent strain? Answer yes or no: '))
-	if fsk_strain_wt == "yes":
-		# if WT strain, nothing needs to be asked (no extra/competing antibiotic)
-		pass
-	elif fsk_strain_wt == "no":
-		# if other WT strain, then ask the name (for protocol) and specify antibiotic
-		# to see if they compete with current selection
-		fsk_other_strain = str(input(f'Name for parent FsK strain used: '))
-		fsk_other_antib_name = str(input(f'Antibiotic name used for parent FsK strain selection: '))
-		if fsk_antib_name == fsk_other_antib_name:
-			print(f'Warning!: Parent strain {fsk_other_strain} also has resistance to {fsk_antib_name}.')
-			sys.exit(1)
-	else:
-		print(f'Please answer yes or no.')
-		sys.exit(1)
-	# Ask for starting date (uses dateutil parser so strict format not required)
-	starting_date = parser.parse(input("Enter experiment's starting date: "))
-	print(starting_date)
-
-
-def calculate_variables():
-	"""Use data input to calculate all desired variables, like plates for transformation, dates, etc. membranes"""
+    # Ask for starting date (uses dateutil parser so strict format not required)
+    starting_date = parser.parse(input("Enter experiment's starting date: "))
+    print(starting_date)
 
 
 def create_table():
-	"""Create table with strains, antibiotics, plates per construct"""
+    """Create table with strains, antibiotics, plates per construct"""
+
+
+data_input()
+print(plate_batch)
+print(membranes)
