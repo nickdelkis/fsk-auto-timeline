d422b08786a84f257182c02184908cb9fd155618
Nick Delkis <51885024+nickdelkis@users.noreply.github.com>
2022-02-16 15:30:42 +0200

    update calculate and table function

--- scripts/script-utilities.py
+++ scripts/script-utilities.py
@@ -43,10 +43,12 @@ class Construct:
         # Ask if fsk strain is WT or not
         self.fsk_wt_strain = str(input(f'Are you using FsK WT (NEK) as parent strain? Answer yes or no: '))
         if self.fsk_wt_strain == "yes":
+            self.fsk_wt_strain = TRUE
             # if WT strain, nothing needs to be asked (no extra/competing antibiotic)
             pass
         # if uses other fsk strain,
         elif self.fsk_wt_strain == "no":
+            self.fsk_wt_strain = FALSE
             # if other WT strain, then ask the name (for protocol) and specify antibiotic
             # to see if they compete with current selection
             self.fsk_other_strain = str(input(f'Name for parent FsK strain used: '))
@@ -68,13 +70,34 @@ def data_input():
     if construct_number > 0:
         print(f"Please enter your constructs' names")
         construct_list = []
+        global agro_antib_name_list
+        agro_antib_name_list = []
+        global agro_antib_conc_list
+        agro_antib_conc_list = []
+        global fsk_antib_name_list
+        fsk_antib_name_list = []
+        global fsk_antib_conc_list
+        fsk_antib_conc_list = []
+        global fsk_wt_strain_list
+        fsk_wt_strain_list = []
+        global fsk_other_strain_list
+        fsk_other_strain_list = []
+        global fsk_other_antib_name_list
+        fsk_other_antib_name_list
         for n in range(construct_number):
             c = f'c{n+1}'
             construct_list.append(c)
         for c in construct_list:
             c = Construct()
             c.add()
-            # TODO: create a dataframe using pandas with each Construct object in a separate column
+            agro_antib_name_list.append(c.agro_antib_name)
+            agro_antib_conc_list.append(c.agro_antib_conc)
+            fsk_antib_name_list.append(c.fsk_antib_name)
+            fsk_antib_conc_list.append(c.fsk_antib_conc)
+            fsk_wt_strain_list.append(c.fsk_wt_strain)
+            fsk_other_strain_list.append(c.fsk_other_strain)
+            fsk_other_antib_name_list.append(c.fsk_other_antib_name)
+
     else:
         print(f'Warning! Please choose a valid construct number.')
         sys.exit(1)
@@ -82,16 +105,6 @@ def data_input():
     # Number of plates per construct to calculate plates needed for transfers and initial transformations
     global plates_per_construct
     plates_per_construct = int(input(f'Number of plates per construct for transformation (e.g. 5): '))
-
-    # Calculate plates per transfer/transformation
-    global plate_batch
-    plate_batch = construct_number * plates_per_construct + 10
-    # CN calculate membranes for transformation
-    global membranes
-    membranes = construct_number * plates_per_construct + 5
-    # calculate IM liquid medium required to go through all necessary steps, plus overage
-    global im
-    im = construct_number*60
 
     # Competent cells available? Will include one week of competent cell preparation if answer is no
     global agro_comp_cells
@@ -107,7 +120,7 @@ def data_input():
 
 
 def round_up(n, decimals=-2):
-    """Round up numbers for media preparation, when calculating volumes."""
+    """Round up numbers for media preparation, when calculating volumes. Default in when doing big volumes, in mL"""
     multiplier = 10 ** decimals
     return ceil(n * multiplier) / multiplier
 
@@ -115,15 +128,30 @@ def round_up(n, decimals=-2):
 def calculate():
     """Calculate all required components for performing the protocol.
     This contains plates, membranes, media, media components. Also uses round_up() to make some volumes look better."""
+# Calculate plates per transfer/transformation
+    global plate_batch
+    plate_batch = construct_number * plates_per_construct + 10
+    # CN calculate membranes for transformation
+    global membranes
+    membranes = construct_number * plates_per_construct + 5
+    # calculate IM liquid medium required to go through all necessary steps, plus overage in mL
+    global im
+    im = round_up(construct_number*60)
 
 
 def create_table():
-    """Create table with strains, antibiotics, plates per construct"""
+    """Create table with strains, antibiotics, plates per construct
+    Use pandas and dataframe function"""
+    table_dict = {"Agrobacterium antibiotic": agro_antib_name_list,
+    "Concentration (ug/mL)": agro_antib_conc_list,
+    "FsK antibiotic": fsk_antib_name_list,
+    "Concentration (ug/mL)": fsk_antib_conc_list,
+    "FsK WT strain as parent": fsk_wt_strain_list,
+    "FsK parent strain name": fsk_other_strain_list,
+    "FsK parent strain antibiotic": fsk_other_antib_name_list}
+    table = pd.DataFrame(table_dict, index= FALSE)
+    return table
 
-
-data_input()
-print(plate_batch)
-print(membranes)
 
 
 def add_week0():
